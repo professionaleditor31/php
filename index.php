438a2818920006fb6d25f5b4319f4d7958e1c137
professionaleditor31 <121536709+professionaleditor31@users.noreply.github.com>
2022-12-28 12:02:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // Comment these lines to hide errors
 error_reporting(E_ALL);
-ini_set('display_errors', 3);
+ini_set('display_errors', 4);
 
 require 'includes/config.php';
 require 'includes/functions.php';
